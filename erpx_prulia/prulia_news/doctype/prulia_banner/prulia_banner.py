c2f8c80046961ccca3704ea1c29f78be417ee270
gentiger <gentiger@gmail.com>
2018-03-28 00:24:00 +0800

    Complete News & Banner

--- erpx_prulia/prulia_news/doctype/prulia_banner/prulia_banner.py
+++ erpx_prulia/prulia_news/doctype/prulia_banner/prulia_banner.py
@@ -8,3 +8,21 @@ from frappe.model.document import Document
 
 class PRULIABanner(Document):
 	pass
+	# def run_post_save_methods(self):
+	# 	if(self.published == 1):
+	# 		banners = []
+	# 		banners = frappe.get_all('PRULIA Banner', fields=['name', 'published'], 
+	# 					filters=[('PRULIA Banner', "published", "=", 1), ('PRULIA Banner', "name", "!=", self.name)],
+	# 					order_by='name')
+	# 		if banners != None:
+	# 			for banner in banners:
+	# 				banner_rec = frappe.get_doc("PRULIA Banner", banner.name);
+	# 				banner_rec.published = 0;
+	# 				banner_rec.save();
+
+@frappe.whitelist()
+def get_banner():
+	banners = frappe.get_all('PRULIA Banner', fields=['name', 'banner_name','image','published'], 
+						filters=[('PRULIA Banner', "published", "=", 1)],
+						order_by='name')
+	return banners
