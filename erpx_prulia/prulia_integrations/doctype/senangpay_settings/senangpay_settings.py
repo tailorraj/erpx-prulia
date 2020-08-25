501b834161e87d73c73d61e06c5591fc16bee051
Tahir Raza <aly_jee@hotmail.com>
2020-08-25 12:54:43 +0500

    Moved payment methods in senangpay settings doctype

--- erpx_prulia/prulia_integrations/doctype/senangpay_settings/senangpay_settings.py
+++ erpx_prulia/prulia_integrations/doctype/senangpay_settings/senangpay_settings.py
@@ -8,3 +8,32 @@ from frappe.model.document import Document
 
 class SenangpaySettings(Document):
 	pass
+
+def create_sha256_signature(key, message):
+    import hmac
+    import hashlib
+    message = message.encode()
+    return hmac.new(str(key), str(message), hashlib.sha256).hexdigest()
+
+def get_payment_link(detail, amount, order_id, name, email, phone):
+    senangpay_settings = frappe.get_doc("Senangpay Settings")
+    if not senangpay_settings.enable:
+        frappe.throw("Please enable Senangpay Settings")
+    secret_key = senangpay_settings.secret_key
+
+    hash_string = str(secret_key)+str(detail)+str(amount)+str(order_id)
+    hashed_value = create_sha256_signature(secret_key, hash_string)
+
+    parameters = {
+        "detail": detail,
+        "amount": amount,
+        "order_id": order_id,
+        "hash": hashed_value,
+        "name": name,
+        "email": email,
+        "phone": phone
+    }
+    import urllib
+    
+    link = str(senangpay_settings.base_url)+"?"+urllib.urlencode(parameters)
+    return link
