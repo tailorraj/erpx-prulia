89ff85ffbb871e2f7647712c9269cc7e55a759f3
root <root@packer-debian-8-amd64.droplet.local>
2021-04-11 09:53:43 +0000

    prod fix

---
+++ erpx_prulia/www/chubb/precache-manifest.9730f66c6e207220ccd735a13fe7e333.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5876f807ef1fedecd937edb949847503",
+    "url": "/chubb/index.html"
+  },
+  {
+    "revision": "8c7759022013f0b798fd",
+    "url": "/chubb/static/css/3.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8ab46261920e0c5a376f",
+    "url": "/chubb/static/css/main.cd9bda41.chunk.css"
+  },
+  {
+    "revision": "c4710bc31b572c217f03",
+    "url": "/chubb/static/js/2.c7a86c75.chunk.js"
+  },
+  {
+    "revision": "8c7759022013f0b798fd",
+    "url": "/chubb/static/js/3.9ebfbd84.chunk.js"
+  },
+  {
+    "revision": "841db4625612d0f21b3d60c176923a8e",
+    "url": "/chubb/static/js/3.9ebfbd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aef2d34c5e4c7299f3b",
+    "url": "/chubb/static/js/4.9e7ba1f9.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/chubb/static/js/4.9e7ba1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81fad3fc81a534d5efd",
+    "url": "/chubb/static/js/5.c9cc410b.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/chubb/static/js/5.c9cc410b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab46261920e0c5a376f",
+    "url": "/chubb/static/js/main.ca3f9293.chunk.js"
+  },
+  {
+    "revision": "35bc5a4f5139135847c4",
+    "url": "/chubb/static/js/runtime-main.e0f99555.js"
+  },
+  {
+    "revision": "2459aad4bbc7fe2278dd2e3651a6d044",
+    "url": "/chubb/static/media/background.2459aad4.jpg"
+  },
+  {
+    "revision": "bcd752146cc5dffe2c3d1504b9794509",
+    "url": "/chubb/static/media/banner.bcd75214.jpg"
+  },
+  {
+    "revision": "61c903117bd1585485f3257658c2f376",
+    "url": "/chubb/static/media/bannerblurr.61c90311.jpg"
+  },
+  {
+    "revision": "03b701ff33b18c5729a67aa8ebfbfa99",
+    "url": "/chubb/static/media/pdfIcon.03b701ff.svg"
+  }
+]);
