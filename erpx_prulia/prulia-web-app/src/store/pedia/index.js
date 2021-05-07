ea06635d7f4d9213ab6fcf53731f9e10ca545a85
neel <neel2292@gmail.com>
2021-05-07 22:53:30 +0800

    pedia listing page

--- erpx_prulia/prulia-web-app/src/store/pedia/index.js
+++ erpx_prulia/prulia-web-app/src/store/pedia/index.js
@@ -2,37 +2,35 @@ import { make } from 'vuex-pathify'
 import axios from 'axios'
 
 const state = {
-    all: []
+  all: []
 }
 
 const getters = {
-    ...make.getters(state),
-    hasPedia: getters => getters.all.length > 0
+  ...make.getters(state),
+  hasPedia: getters => getters.all.length > 0
 }
 const mutations = {
-    ...make.mutations(state)
+  ...make.mutations(state)
 }
 
 const actions = {
-    ...make.actions(state),
-    load({ commit }) {
-        // axios
-        //     .get(
-        //         `/api/method/erpx_prulia.prulia_news.doctype.prulia_newsletter.prulia_newsletter.get_newsletter_list`
-        //     )
-        //     .then(({ data }) => {
-        //         let { message } = data
-        //         commit('SET_ALL', message || [])
-        //     })
+  ...make.actions(state),
+  load({ commit }) {
+    axios
+      .get(
+        `/api/method/erpx_prulia.prulia_news.doctype.prulia_pedia.prulia_pedia.get_pedia_posts`
+      )
+      .then(({ data }) => {
         let { message } = data
         commit('SET_ALL', message || [])
-    }
+      })
+  }
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    getters,
-    actions
+  namespaced: true,
+  state,
+  mutations,
+  getters,
+  actions
 }
