d78db35bb41c40469be6c9c76c81602a12c90a52
Neel <neel2292@gmail.com>
2021-05-08 09:26:44 +0000

    proper-pediatest

---
+++ erpx_prulia/prulia_pediatest/doctype/prulia_pediatest/prulia_pediatest.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Alpha Herald Management and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PRULIA PediaTest', {
+	refresh: function(frm) {
+
+	}
+});
