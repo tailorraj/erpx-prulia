7d8f0ba02e62023e0e287934f0468b4a2620f937
neel <neel@rocketbots.io>
2021-05-09 12:06:01 +0800

    pedia listing

--- erpx_prulia/prulia-web-app/src/views/pedia/index.vue
+++ erpx_prulia/prulia-web-app/src/views/pedia/index.vue
@@ -1,6 +1,35 @@
 <template>
   <v-container>
-    <v-row v-if="all.length"> </v-row>
+    <v-row v-if="all.length" class="primary">
+      <v-col
+          align-self="center"
+          class="pa-6"
+          cols="12"
+          sm="6"
+          md="4"
+          v-for="(pedia, index) in all"
+          :key="`pedia-${index}`"
+      >
+        <v-card
+            min-height="100"
+            class="dusk"
+            hover
+            :to="{ name: 'PediaDetails', params: { id: pedia.name } }"
+        >
+          <v-card-subtitle class="pb-0">
+            <h3>
+              <text-truncate>{{ pedia.title }}</text-truncate>
+            </h3>
+          </v-card-subtitle>
+          <v-card-actions class="">
+            <v-spacer />
+            <span class="subtitle-2 pa-2 text--darken-4">
+              {{ pedia.published_date | formatDate('DD MMM YYYY') }}
+            </span>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
     <v-row
       v-else
       justify="center"
@@ -28,6 +57,7 @@ export default {
     ...mapGetters('pedia', ['all'])
   },
   mounted() {
+    this.$store.dispatch('pedia/load')
     this.$store.dispatch('pedia/loadMeta')
   }
 }
