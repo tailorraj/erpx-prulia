09be05c583848eb088c94719f31e15cec49bfb54
Neel <neel2292@gmail.com>
2021-05-16 04:26:09 +0000

    Create pedia functionality

--- erpx_prulia/prulia_pedia/doctype/prulia_pedia/prulia_pedia.py
+++ erpx_prulia/prulia_pedia/doctype/prulia_pedia/prulia_pedia.py
@@ -3,8 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, json
+from frappe.utils import now_datetime
 from frappe.model.document import Document
+from erpx_prulia.prulia_members.doctype.prulia_member.prulia_member import mobile_member_login
 
 class PRULIAPedia(Document):
 	pass
@@ -27,31 +29,16 @@ def get_pedia_posts():
 def create_new_post(data):
     dat = json.loads(data)
     doc = frappe.new_doc("PRULIA Pedia")
+
     doc.flags.ignore_permissions = True
     doc.flags.ignore_mandatory = True
-    doc.title = dat.get('title')
-    doc.published = dat.get('published')
-    #doc.published_date = frappe.datetime.now_datetime()
-    doc.prudential_id = dat.get('prudential_id')
-    doc.full_name = dat.get('full_name')
-    doc.email = dat.get('email')
-    doc.am_um_assist = dat.get('am_um_assist')
-    doc.am_um_name = dat.get('am_um_name')
-    doc.apm_assist = dat.get('apm_assist')
-    doc.apm_name = dat.get('apm_name')
-    doc.pamb_assist = dat.get('pamb_assist')
-    doc.pamb_name = dat.get('pamb_name')
-    doc.qa_name = dat.get('qa_name')
-    doc.agency_code = dat.get('agency_code')
-    doc.region = dat.get('region')
-    doc.category = dat.get('category')
-    doc.other_cat = dat.get('other_cat')
-    doc.policy_no = dat.get('policy_no')
-    doc.assured_name = dat.get('assured_name')
-    doc.product_name = dat.get('product_name')
-    doc.medical_plan = dat.get('medical_plan')
-    doc.med_claim_other = dat.get('med_claim_other')
-    doc.reason_of_declined = dat.get('reason_of_declined')
+
+    for key in dat:
+        doc.set(key, dat.get(key))
+    doc.published_date = now_datetime()
+    member = mobile_member_login()
+    doc.prudential_id = member.name
+
     doc.save()
 
     return doc
